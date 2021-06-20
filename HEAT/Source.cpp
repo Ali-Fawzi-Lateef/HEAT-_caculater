6ea5b1b0d5abd3eba20dee57f02da8c0f5ffda9c
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-06-20 23:42:21 +0300

    Update Source.cpp

--- HEAT/Source.cpp
+++ HEAT/Source.cpp
@@ -42,16 +42,16 @@ int main()
 		std::cin >> opt;//input from the user
 		switch (opt) {//switching options
 		case 1:
-			HEAT.Kelvin();
+			HEAT.Kelvin();//invoke the function
 			break;
 		case 2:
-			HEAT.fahrenheit();
+			HEAT.fahrenheit();//invoke the function
 			break;
 		case 3:
-			HEAT.CelsiusF();
+			HEAT.CelsiusF();//invoke the function
 			break;
 		case 4:
-			HEAT.CelsiusK();
+			HEAT.CelsiusK();//invoke the function
 			break;
 		case 0:
 			std::cout << "GoodBye ^_^\a";//if the user chosen to EXIT.  // \a for alarm sound.
