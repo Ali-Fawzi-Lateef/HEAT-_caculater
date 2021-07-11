27c8619e0d378cd024cb0121fecd52b93afd2398
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-07-11 23:47:59 +0300

    Update Source.cpp

--- HEAT/Source.cpp
+++ HEAT/Source.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-class Heat {//source class
+class Heat {//source class.
 public:
-	double num;//declearing num
+	double num;//declearing num.
 	void Kelvin() {//function for changing the haet from Celsius to Kelvin.
 		std::cout << "Enter the value: ";
 		std::cin >> num;//input from the user.
